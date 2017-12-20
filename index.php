87bda6afed4dcfe77b20de7d847d1236d0d1b135
Andrey Pestov <apestov@rooxteam.com>
2017-12-20 10:03:52 +0300

    fix: disable mobile app hint till client approvement

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+        <!---
 	<meta name="apple-itunes-app" content="app-id=557857165" />
 	<meta name="google-play-app" content="app-id=ru.raiffeisennews" />
+        -->
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=UA-111419738-1"></script>
