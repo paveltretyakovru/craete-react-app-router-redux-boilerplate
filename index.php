cf89e361dc83dc33aa178ea7fa82b6743db6fa3b
Andrey Pestov <apestov@rooxteam.com>
2017-12-22 11:27:46 +0300

    fix: remove items that are not part of preload screen with logo

--- index.php
+++ index.php
@@ -54,26 +54,19 @@
 						<p class="scene-content__subtitle">Ваш браузер не поддерживается</p>
 					</div>
 				</div>
+				<footer class="footer">
+					<span class="copyright">© 2003–2017 АО «Райффайзенбанк». </span>
+				</footer>
 			</noscript>
 			<div class="scene-content initial-load">
 				<div class="content-animation">
-					<h1><span class="scene-content__welcome">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span></h1>
+					<h1><span class="scene-content__welcome">&nbsp;</span></h1>
 					<p class="scene-content__subtitle" style="visibility: hidden">За&nbsp;окном морозный декабрь,близятся
 						новогодние праздники, а&nbsp;значит
 						самое время подвести итоги
 						уходящего 2017&nbsp;года.</p>
 				</div>
-				<div class="container-btn" style="visibility: hidden">
-					<a href="" class="btn btn-default" data-action="top-scroll" data-stat-action="go">Поехали?</a>
-				</div>
 			</div>
-			<div class="scene-bottom initial-load" style="visibility: hidden">
-				<img class="scene-bottom__img hidden-xs" data-src="assets/i/intro.png" alt="">
-				<img class="scene-bottom__img visible-xs" src="assets/i/intro_m.png" alt="">
-			</div>
-			<footer class="footer">
-				<span class="copyright">© 2003–2017 АО «Райффайзенбанк». </span>
-			</footer>
 		</section>
 	</div>
 </main>
