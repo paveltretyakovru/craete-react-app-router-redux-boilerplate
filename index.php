cc794aab1e1e1ed542aa7b39ff4077c776147478
mnagaev <mnagaev@roox.ru>
2017-12-26 22:57:17 +0300

    Preload api request and name

--- index.php
+++ index.php
@@ -16,13 +16,14 @@
             var pathParts = window.location.pathname.split("/"),
                 lastSegment = pathParts.pop() || pathParts.pop(),
                 preloadInfo = fetch && "function" == typeof fetch && fetch("/ny2018/webapi-1/info/" + lastSegment + "/").then(function(response){
-                    if (response.status !== 200) {
+                    if (response.status == 200) {
                         return response.json();
                     } else {
                         return null;
 					}
 				}).then(function(data) {
 				    console.log(data.data.jsonBody.clientName + ", здравствуйте!");
+				    return data;
 				})
         } catch (t) {}
     </script>
