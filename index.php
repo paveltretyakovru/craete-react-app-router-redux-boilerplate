a44f71851ede70c6c6e9709ac5add69b1cea1fbe
mnagaev <mnagaev@roox.ru>
2018-09-19 15:14:36 +0300

    Fix some strings

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ru" class="layout-html">
 <head>
-	<title>Итоги уходящего 2017 года</title>
+	<title>Лидеры России</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
