42251b01e716da5d871b2d4f7e252440d1cce8d2
Pavel Tretyakov <ptretyakov@roox.ru>
2018-09-26 19:17:06 +0300

    Code refactoring

---
+++ src/landing/LandingConstants.js
@@ -0,0 +1,17 @@
+export const fullpageOptions = {
+  anchors: [
+    'dukovAppealSection',
+    'informationSection',
+    'carouselSection',
+    'callToActionSection',
+  ],
+  navigation: false,
+  sectionClassName: 'section',
+};
+
+export const carouselOptions = {
+  showArrows: false,
+  showThumbs: false,
+  showStatus: false,
+  emulateTouch: true, 
+}
--- src/landing/LandingContainer.jsx
+++ src/landing/LandingContainer.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {push} from 'connected-react-router';
 import {connect} from 'react-redux';
 import {Carousel} from 'react-responsive-carousel';
 import {bindActionCreators} from 'redux';
 import {SectionsContainer} from 'react-fullpage';
 
+// Components
 import {DukovAppealSectionComponent} from './shared/components/sections/DukovAppeal/DukovAppealSectionComponent';
 import {InformationSectionComponent} from './shared/components/sections/Information/InformationSectionComponent';
 import {DukovReviewSectionComponent} from './shared/components/sections/DukovReview/DukovReviewSectionComponent';
@@ -13,44 +14,34 @@ import {CallToActionSectionComponent} from './shared/components/sections/CallToA
 import {WinnerElbrusSectionComponent} from './shared/components/sections/WinnerElbrus/WinnerElbrusSectionComponent';
 import {WinnerInterviewSectionComponent} from './shared/components/sections/WinnerInterview/WinnerInterviewSectionComponent';
 
+// Constants
+import {fullpageOptions, carouselOptions} from './LandingConstants';
+
+// Styles
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 
-const fullpageOptions = {
-  anchors: [
-    'dukovAppealSection',
-    'informationSection',
-    'carouselSection',
-    'callToActionSection',
-  ],
-  navigation: false,
-  sectionClassName: 'section',
-};
-
-const carouselOptions = {
-  showArrows: false,
-  showThumbs: false,
-  showStatus: false,
-  emulateTouch: true, 
+class LandingComponent extends Component {
+  render() {
+    return (
+      <SectionsContainer {...fullpageOptions}>
+        <DukovAppealSectionComponent />
+        <InformationSectionComponent />
+    
+        <Carousel {...carouselOptions}>
+          <DukovReviewSectionComponent />
+          <DeminReviewSectionComponent />
+          <WinnerInterviewSectionComponent />
+          <WinnerElbrusSectionComponent />
+        </Carousel>
+    
+        <CallToActionSectionComponent />
+      </SectionsContainer>
+    );
+  }
 }
 
-const LandingComponent = props => (
-  <SectionsContainer {...fullpageOptions}>
-    <DukovAppealSectionComponent />
-    <InformationSectionComponent />
-
-    <Carousel {...carouselOptions}>
-      <DukovReviewSectionComponent />
-      <DeminReviewSectionComponent />
-      <WinnerInterviewSectionComponent />
-      <WinnerElbrusSectionComponent />
-    </Carousel>
-
-    <CallToActionSectionComponent />
-  </SectionsContainer>
-);
-
 const mapDispatchToProps = dispatch => bindActionCreators({
-  changePage: () => push('/portal')
+  goToPortal: () => push('/portal')
 }, dispatch);
 
 export default connect(null, mapDispatchToProps)(LandingComponent);
