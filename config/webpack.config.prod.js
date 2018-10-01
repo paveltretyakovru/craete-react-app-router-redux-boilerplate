061ccbd1585a3fdc6fd638d6e3d5d784f125b11b
Pavel Tretyakov <ptretyakov@roox.ru>
2018-10-01 11:54:41 +0300

    Updated webpack.config.prod.js. Added SCSS test rules

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -107,6 +107,15 @@ module.exports = {
   module: {
     strictExportPresence: true,
     rules: [
+      {
+        test: /\.scss$/,
+        include: paths.appSrc,
+        loaders: [
+          require.resolve('style-loader'),
+          require.resolve('css-loader'),
+          require.resolve('sass-loader')
+        ]
+      },
       // TODO: Disable require.ensure as it's not a standard language feature.
       // We are waiting for https://github.com/facebookincubator/create-react-app/issues/2176.
       // { parser: { requireEnsure: false } },
