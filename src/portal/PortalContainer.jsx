8f5931836d585be59f48d8916e916ba11e1e98c1
Pavel Tretyakov <ptretyakov@roox.ru>
2018-10-02 20:01:25 +0300

    Makeuped portal skeleton, created header and checkbox component

--- src/portal/PortalContainer.jsx
+++ src/portal/PortalContainer.jsx
@@ -3,16 +3,33 @@ import {push} from 'connected-react-router';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 
+// Actions
 import {updateTabIndex} from './portalActions';
 
+
+// Styles
+import './PortalContainer.scss';
+import { PortalChecklistComponent } from './shared/components/checklist/PortalChecklistComponent';
+import { PortalHeaderComponent } from './shared/components/header/PortalHeaderComponent';
+
 const PortalContainer = props => (
-  <div>
-    <h1>Portal Container</h1>
-    <div>
-      <p>Tab index: { props.tabIndex }</p>
-      <button onClick={() => props.updateTabIndex(5)}>Update tab index</button>
-      <button onClick={() => props.changePage()}>Go to landing</button>
-    </div>
+  <div className="portal">
+    <aside className="portal__left-side">
+      <PortalChecklistComponent />
+    </aside>
+
+    <aside className="portal__right-side">
+      Right side
+    </aside>
+
+    <header className="portal__header">
+      <PortalHeaderComponent />
+    </header>
+
+    <main className="portal__content">
+      Main content
+    </main>
+
   </div>
 );
 
---
+++ src/portal/shared/components/checklist/PortalChecklistComponent.jsx
@@ -0,0 +1,75 @@
+import React, {Component} from 'react';
+
+// Constants
+import { PORTAL_CHECKLIST_LINKS, PORTAL_CHECKLIST_TITLE, PORTAL_CHECKLIST_ITEMS } from './PortalChecklistConstants';
+
+// Styles
+import './PortalChecklistComponent.scss';
+
+// Images
+import portalLogoImage from './shared/assets/images/portal-logo.png';
+import portalCheckboxImage from './shared/assets/images/portal-checkbox.png';
+import portalCheckboxCheckedImage from './shared/assets/images/portal-checkbox-checked.png';
+
+export class PortalChecklistComponent extends Component {
+  render() {
+    return (
+      <div className="portal-checklist">
+
+        <div className="portal-checklist__logo">
+          <img src={portalLogoImage} alt=""/>
+        </div>
+
+        <div className="portal-checklist__list">
+          <div className="portal-checklist__list-title">
+          {PORTAL_CHECKLIST_TITLE}
+          </div>
+          <ul className="portal-checklist__list-items">
+            {
+              PORTAL_CHECKLIST_ITEMS.map((checkbox) => {
+                return (
+                  <li
+                    key={`portal-checkbox-item-${checkbox.id}`}
+                    className={
+                      checkbox.checked
+                        ? 'portal-checklist__list-item portal-checklist__list-item_checked'
+                        : 'portal-checklist__list-item portal-checklist__list-item_unchecked'
+                    }
+                  >
+                      {
+                        checkbox.checked
+                          ? <img
+                              src={portalCheckboxCheckedImage}
+                              className="portal-checklist__list-item-image" />
+                          : <img
+                              src={portalCheckboxImage}
+                              className="portal-checklist__list-item-image" />
+                      }
+                    <div className="portal-checklist__list-item-value">
+                      {checkbox.value}
+                    </div>
+                  </li>
+                );
+              })
+            }
+          </ul>
+        </div>
+
+        <div className="portal-checklist__links">
+          {PORTAL_CHECKLIST_LINKS.map((link) => {
+            return (
+              <a href={link.src} key={`portal-checklist-links-${link.id}`}>
+                {link.value}
+              </a>
+            )
+          })}
+        </div>
+
+      </div>
+    )
+  }
+
+  handleInputChange(event) {
+    console.log('Change event', event);
+  }
+}
