2b73ab2584f07e3b2969dec65680e6e698b7669b
Pavel Tretyakov <ptretyakov@roox.ru>
2018-10-08 17:24:56 +0300

    Fixed PD-10865. Updated CallToActionMobileComponent.jsx

--- src/landing/shared/components/mobile/CallToAction/CallToActionMobileComponent.jsx
+++ src/landing/shared/components/mobile/CallToAction/CallToActionMobileComponent.jsx
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 
 import './CallToActionMobileComponent.scss';
-import { WINNER_ELBRUS_TITLE_WINNERS, WINNER_ELBRUS_TITLE } from '../../sections/WinnerElbrus/WinnerElbrusSectionConstants';
-import { CALL_TO_ACTION_DESCRIPTION, CALL_TO_ACTION_LINK_DESCRIPTION, CALL_TO_ACTION_LINK, CALL_TO_ACTION_COPYRIGHT } from '../../sections/CallToAction/CallToActionConstants';
+import { CALL_TO_ACTION_DESCRIPTION, CALL_TO_ACTION_LINK_DESCRIPTION, CALL_TO_ACTION_LINK, CALL_TO_ACTION_COPYRIGHT, CALL_TO_ACTION_TITLE_SPECIAL, CALL_TO_ACTION_TITLE } from '../../sections/CallToAction/CallToActionConstants';
 
 export class CallToActionMobileComponent extends Component {
   render() {
@@ -16,8 +15,8 @@ export class CallToActionMobileComponent extends Component {
       <div className="call-to-action-mobile">
 
         <div className="call-to-action-mobile__title gpn-slideshow-mobile__title">
-          <span className="blue-text">{WINNER_ELBRUS_TITLE_WINNERS}</span>
-          &nbsp;{WINNER_ELBRUS_TITLE}
+          <span className="blue-text">{CALL_TO_ACTION_TITLE_SPECIAL}</span>
+          &nbsp;{CALL_TO_ACTION_TITLE}
         </div>
         <div className="call-to-action-mobile__text gpn-slideshow-mobile__text">
           {description}
