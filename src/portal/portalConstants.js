9a4d83b3ad4458425b538f9f2f421fcc7a13b0ae
DaryaBobko <bobko.dv@gmail.com>
2018-10-08 19:39:16 +0300

    Fixed bugs

--- src/portal/portalConstants.js
+++ src/portal/portalConstants.js
@@ -1,3 +1,3 @@
 export const CHANGE_TAB_INDEX = 'CHANGE_TAB_INDEX';
 export const PORTAL_BLUE_BLOCK_LABEL = 'Пройдите опрос о работе площадки и получите доступ к нашей личной библиотеке!';
-export const PORTAL_BLUE_BLOCK_LINK_LABEL = 'Приступить к опросу';
+export const PORTAL_BLUE_BLOCK_LINK_LABEL = 'Скоро...';
